805064f8a45fab615aabf6753a5a3274ffdcd81a
Junhyun <junj7878@knu.ac.kr>
2019-05-15 22:53:48 +0900

    change

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 from model import *
 import matplotlib.pyplot as plt
 
-model = Model(2, 8, 1, activation='sigmoid')
+model = Model(2, 4, 1, activation='sigmoid')
 
 # Test XOR problem!
-x = np.array([0, 0], [0, 1], [1, 0], [1, 1])
+x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 y = np.array([0, 1, 1, 0])
 
 epoch = 20
 train_loss = []
 
 for i in range(epoch):
-    result = model.forward(x)
-    loss = (np.square(result - y)).mean()
-    backprop = model.backward(loss)
-    model.update()
+    print('Epoch :', i+1)
+    for k, x_ in enumerate(x): # Batch size = 1
+        result = model.forward(x_)
+        loss = (np.square(result - y[k])).mean()
+        backprop = model.backward(loss)
+        model.update()
 
     train_loss.append(loss)
 
 # Testing..
-print(model.forward(x))
+print(model.forward(np.array([1,0])))
 
 plt.plot(train_loss)
 plt.show()
